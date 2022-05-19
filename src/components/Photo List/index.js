7f81c16e9f8dca1b898359e0bb2eeb77a12d8e5c
rharris529 <jordandine@gmail.com>
2022-05-18 22:50:17 -0500

    images not loading for modal

--- src/components/Photo List/index.js
+++ src/components/Photo List/index.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
-// import Modal from "../Modal";
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState(); 
+
     const [photos] = useState([
         {
             name: 'Grocery aisle',
@@ -102,20 +106,27 @@ const PhotoList = ({ category }) => {
 
     const currentPhotos = photos.filter(photo => photo.category === category);
 
+    const toggleModal = (image, i) => {
+      setCurrentPhoto({...image, i})
+      setIsModalOpen(isModalOpen);
+    }
+
     return (
         <div>
+          {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>}
             <div className='flex-row'>
                 {currentPhotos.map((image, i) => (
                     <img 
                     src={require(`../../assets/small/${category}/${i}.jpg`).default}
                     alt={image.name}
                     className="img-thumbnail mx-1"
+                    onClick={() => toggleModal(image, i)}
                     key={image.name}
                     />
                 ))}
             </div>
         </div>
     );
-}
+};
 
 export default PhotoList;
