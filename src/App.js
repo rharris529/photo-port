71fe7e8488ac5141b58bb282f74a92ffe43a31d3
rharris529 <jordandine@gmail.com>
2022-05-18 21:36:09 -0500

    added conditional styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -24,12 +27,19 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+          {!contactSelected ? (
+            <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+            </>
+          ) : (
+            <ContactForm></ContactForm>
+          )}
         </div>
       </main>
     </div>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,37 @@
 import React, {useState} from 'react';
+import {validateEmail} from "../../utils/helpers";
 
 function ContactForm() {
+
+    const [errorMessage, setErrorMessage] = useState('');
 
     const [formState, setFormState] = useState({ name: '', email: '', message: ''});
 
     const {name, email, message} = formState;
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value})
+        if(e.target.name === "email") {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid condition statement
+            if(!isValid) {
+                setErrorMessage('Your email is invalid');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if(!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value});
+        }
+
+        console.log('errorMessage', errorMessage);
     };
 
     function handleSubmit(e) {
@@ -21,16 +45,21 @@ function ContactForm() {
           <form id="contact-form" onSubmit={handleSubmit}>
             <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" defaultValue={name}    onChange={handleChange} name="name" />
+            <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
             </div>
             <div>
             <label htmlFor="email">Email address:</label>
-            <input type="email" defaultValue={email}onChange={handleChange} name="email" />
+            <input type="email" defaultValue={email}onBlur={handleChange} name="email" />
             </div>
             <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+            <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
             </div>
+            {errorMessage && (
+                <div>
+                    <p className='error-text'>{errorMessage}</p>
+                </div>
+            )}
             <button type='submit'>Submit</button>
           </form>
         </section>
